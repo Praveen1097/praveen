5033ef21f1dda537ac89565caca2f75eb932561e
Praveen1097 <50832186+Praveen1097@users.noreply.github.com>
2019-05-21 11:46:36 +0530

    Add files via upload

---
+++ neg.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+void main()
+
+{
+
+    int a;
+    
+scanf("%d",&a);
+    
+if(a>0)
+    
+printf("Positive",a);
+    
+else if(a<0)
+    
+printf("Negative",a);
+    
+else 
+    
+printf("Zero");
+
+    
+return 0;
+
+}
