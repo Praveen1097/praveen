5af3ea0bc7715636d84a8a3fd4257ffd0f462507
Praveen1097 <50832186+Praveen1097@users.noreply.github.com>
2019-05-21 13:30:10 +0530

    Add files via upload

---
+++ odd or even.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+
+
+void main()
+
+
+{
+
+
+int n;
+
+
+scanf("%d",&n);
+
+
+if(n%2==0)
+
+
+printf("Even",n);
+
+
+else if(n%2==1)
+
+printf("Odd",n);
+
+else
+printf("Invalid");
+
+
+}
