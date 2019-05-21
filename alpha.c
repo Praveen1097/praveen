455463fc8bbcebc142c82881fc9ed4ae38d6e6bf
Praveen1097 <50832186+Praveen1097@users.noreply.github.com>
2019-05-21 14:11:42 +0530

    Add files via upload

---
+++ alpha.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+    char c;
+    scanf("%c",&c);
+    if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+    {
+        printf("Alphabet",c);
+    }
+    else
+    printf("No");
+}
