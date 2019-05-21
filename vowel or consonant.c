3c1fdabb6c967dee67f9cea84d1deb44f80419a2
Praveen1097 <50832186+Praveen1097@users.noreply.github.com>
2019-05-21 13:49:30 +0530

    Add files via upload

---
+++ vowel or consonant.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char c;
+scanf("%c",&c);
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+v{
+printf("Vowel",c);
+}
+else if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+{
+printf("Consonant",c);
+}
+else
+printf("Invalid");
+}
